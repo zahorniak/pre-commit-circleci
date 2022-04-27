30edd60565f5d421b88bd07701a6c70fecc26179
Ferenc Géczi <ferenc.geczi@ibm.com>
2022-04-27 00:00:00 +0000

    chore: fix mixed ident

--- circleci_validate.sh
+++ circleci_validate.sh
@@ -13,7 +13,7 @@ then
 fi
 
 if ! eMSG=$(circleci config validate "$@" -c .circleci/config.yml); then
-	echo "CircleCI Configuration Failed Validation."
-	echo "${eMSG}"
-	exit 1
+    echo "CircleCI Configuration Failed Validation."
+    echo "${eMSG}"
+    exit 1
 fi
