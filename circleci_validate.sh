d06811aaccad270912d37aefb8b697541c1f1ef4
Ferenc Géczi <ferenc.geczi@ibm.com>
2022-04-27 00:00:00 +0000

    feat: skip validation on circleci

--- circleci_validate.sh
+++ circleci_validate.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ -n $CIRCLECI ]]; then
+    echo "Circleci environment detected, skipping validation."
+    exit 0
+fi
+
 if ! command -v circleci &> /dev/null
 then
     echo "Circleci CLI could not be found. Install the latest CLI version https://circleci.com/docs/2.0/local-cli/#installation"
