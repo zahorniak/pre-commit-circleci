1c2be1313a0c892023e790343b909c86d1854b3b
Ferenc- <Ferenc-@users.noreply.github.com>
2022-04-27 12:56:22 +0200

    Various small improvements (#7)
    * feat: skip validation on circleci
    * fix: fix shellcheck findings
    * chore: fix mixed ident

--- circleci_validate.sh
+++ circleci_validate.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ -n $CIRCLECI ]]; then
+    echo "Circleci environment detected, skipping validation."
+    exit 0
+fi
+
 if ! command -v circleci &> /dev/null
 then
     echo "Circleci CLI could not be found. Install the latest CLI version https://circleci.com/docs/2.0/local-cli/#installation"
     exit 1
 fi
 
-if ! eMSG=$(circleci config validate $@ -c .circleci/config.yml); then
-	echo "CircleCI Configuration Failed Validation."
-	echo $eMSG
-	exit 1
+if ! eMSG=$(circleci config validate "$@" -c .circleci/config.yml); then
+    echo "CircleCI Configuration Failed Validation."
+    echo "${eMSG}"
+    exit 1
 fi
